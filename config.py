551d2e3648e69a8855f0f4888b1281d199cdc7ee
Fruitcakee-live <mayanklbs0987654321@gmail.com>
2024-07-23 11:35:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7345892400:AAFvkTPG7hjxVKy7WJWda-g21MAxng9Zp5w")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("fruitXddos")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6638180102")
 
 
 
